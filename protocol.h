d4c6866c121981dc91e36c20381496d4917bbf58
xqiu35 <xqiu35@gmail.com>
2015-11-17 12:55:43 -0500

    commit

---
+++ protocol.h
@@ -0,0 +1,11 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+const char LED = 0x00;
+const char TURN_LEFT = 0x01;
+const char TURN_RIGHT = 0x02;
+const char MOVE_FORWARD = 0x03;
+const char MOVE_BACKWARD = 0x04;
+const char STOP = 0x05;
+
+#endif //PROTOCOL_H
