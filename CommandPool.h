be819d5f5b9bd339e81671a3b532dd743e387575
xqiu35 <xqiu35@gatech.edu>
2015-12-10 15:31:08 -0500

    Update CommandPool.h

--- CommandPool.h
+++ CommandPool.h
@@ -5,25 +5,29 @@
 #include <vector>
 #include <string>
 
-typedef std::map<char,Command*>::const_iterator mapIterator;
-
+/** CommandPool class.
+ *  Author Xiaofei Qiu
+ */
+ 
 class CommandPool
 {   
 public:
     CommandPool(){}
     ~CommandPool();
     
-    /* init pool, push all commands to _pool*/
-    void init(const size_t& num_entries = 2);  
+    /** init pool, push all commands to _pool*/
+    void init();  
     
-    /* this will return a pointer to the command    
-       first four bit is used to determine the command
-       last four bit is used to determine the speed 
+    /** This will return a command object
+      * Check input commands in protocol.h file  
+      * use first 3 bits are used to determine the command
+      * use 3th bit for speed direction
+      * use 4th bit and 5th bit to define the speed
     */  
     Command* getCommand(const char& cmdstr);            
                                                         
         
 private:
-    /* container holds all commands */
+    /** container holds all commands */
     std::map<char,Command*> _pool;                      
 };
